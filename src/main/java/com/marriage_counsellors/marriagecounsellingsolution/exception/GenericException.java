e812d75dc8f88d68149d8c286a7ebe00ad389ac5
stevedeey <stvolutosin69@gmail.com>
2021-11-29 14:53:07 +0100

    create custom excetion

--- src/main/java/com/marriage_counsellors/marriagecounsellingsolution/exception/GenericException.java
+++ src/main/java/com/marriage_counsellors/marriagecounsellingsolution/exception/GenericException.java
@@ -1,2 +1,17 @@
-package com.marriage_counsellors.marriagecounsellingsolution.exception;public class GenericExcetion {
+package com.marriage_counsellors.marriagecounsellingsolution.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+@Getter
+public class GenericException {
+    private final String message;
+    private final ZonedDateTime timeStamp;
+
+    public GenericException(String message,  ZonedDateTime timeStamp) {
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
 }
--- src/main/java/com/marriage_counsellors/marriagecounsellingsolution/exception/ResourceNotFoundException.java
+++ src/main/java/com/marriage_counsellors/marriagecounsellingsolution/exception/ResourceNotFoundException.java
@@ -1,2 +1,21 @@
-package com.marriage_counsellors.marriagecounsellingsolution.exception;public class ResourceNotFoundException {
+package com.marriage_counsellors.marriagecounsellingsolution.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = -7034897190745766939L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
